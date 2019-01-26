ada41ddc83ccfffffde7c8f3f8d6723e7490c2ce
NorthWardTop <2394783277@qq.com>
2019-01-26 13:56:41 +0800

    完成aio-copy主函数

---
+++ copy.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>//文件状态
+#include <sys/types.h>
+#include <time.h>
+#include <semaphore.h>
+#include <dirent.h>
+#include <errno.h>
+#include <aio.h>
+
+
+int main(int argc, char const *argv[])
+{
+
+    
+    return 0;
+}
+
+
+
